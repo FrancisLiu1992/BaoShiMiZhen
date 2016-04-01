c7ac4e3e1dae6a1bceeadb0eb478a14f6faa5185
FrancisLiu1992 <sdk@SDKdeMac-mini-4.local>
2016-04-01 15:30:00 +0800

    assets

---
+++ Assets/Scripts/Object/PLayerInfo.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 玩家信息 显示对象脚本
+/// </summary>
+public class PLayerInfo : MonoBehaviour
+{
+
+    public static PLayerInfo Info;      // infomations of player
+
+    public static Player MapPlayer;     // player object
+
+    public static byte MODE;            // mode : Arcade or Classic 
+
+    public static int BACKGROUND;       // background of mode
+
+    public int Score;
+
+    public const string KEY_CLASSIC_HISCORE = "classichightscore";
+
+    public TextMesh textlv;
+
+    void Awake()
+    {
+        Info = this;
+		Debug.Log("+++++sss" + MapPlayer);
+		Debug.Log("+++++sss" + MapPlayer.Background);
+        BACKGROUND = MapPlayer.Background;
+
+    }
+
+    void Start()
+    {
+        Score = 0;
+        EffectSpawner.effect.SetLevel(MapPlayer.Level);
+        EffectSpawner.effect.SetBest(MapPlayer.HightScore);
+        EffectSpawner.effect.SetScore(Score);
+        textlv.text = MapPlayer.Level.ToString();
+    }
+}
